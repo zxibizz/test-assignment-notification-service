c76c25e923b1a508b151d0f0a0897385788261de
Roman Lee <romanlee1996@gmail.com>
2022-12-19 00:08:08 +0600

    Removed api authentication and permission

--- config/settings/base.py
+++ config/settings/base.py
@@ -307,11 +307,8 @@ SOCIALACCOUNT_FORMS = {
 # -------------------------------------------------------------------------------
 # django-rest-framework - https://www.django-rest-framework.org/api-guide/settings/
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.TokenAuthentication",
-    ),
-    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_AUTHENTICATION_CLASSES": [],
+    "DEFAULT_PERMISSION_CLASSES": [],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
